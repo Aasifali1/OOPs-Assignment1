5bb633fc30ab40fcb5c9795d06b802328091131c
Aasif Ali <aasif.ali@knoldus.com>
2021-08-21 13:01:56 +0530

    OOPs assignment-1 done

---
+++ src/main/scala/com/knoldus/oopsAssignment1/DoubleQueue.scala
@@ -0,0 +1,27 @@
+package com.knoldus.oopsAssignment1
+
+class DoubleQueue extends Queue {
+
+  /* Overriding the enqueue method of Queue trait.
+  *  This method will enqueue the  item to the queue in FIFO Order after doubling them
+  *  The item will be enqueued in the last of the list and front and rear will be increased.
+  * */
+  override def enqueue(item:Int): Unit = {
+    if(front == -1 && rear == -1)  // If the queue is empty then both the values of front and rear will be increased by one.
+      {
+        front = front + 1
+        rear = rear + 1
+        bufferItem += item * 2
+        println("Item enqueued")
+        val list = bufferItem.toList
+        println(list)
+      }
+    else {      // If the queue is not empty then the only value of rear will be increased by one.
+      rear = rear + 1
+      bufferItem += item * 2
+      println("Item enqueued")
+      val list = bufferItem.toList
+      println(list)
+    }
+  }
+}
